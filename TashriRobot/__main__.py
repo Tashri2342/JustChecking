d5792ed7d57326ff1d91bedba9c18b75c3424893
Tashri2342 <108603052+Tashri2342@users.noreply.github.com>
2023-07-10 13:49:27 +0530

    Update __main__.py

--- TashriRobot/__main__.py
+++ TashriRobot/__main__.py
@@ -1318,7 +1318,7 @@ def main():
     dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
     dispatcher.add_handler(donate_handler)
-    dispatcher.add_handler(mukeshrobot_main_handler)
+    dispatcher.add_handler(tashrirobot_main_handler)
     dispatcher.add_error_handler(error_callback)
                                                    
                                       
