9df3649d5ef631f1e84269cd79562e6b3da33a9d
Tashri2342 <108603052+Tashri2342@users.noreply.github.com>
2023-07-21 18:58:57 +0530

    Update writetool.py

--- TashriRobot/modules/writetool.py
+++ TashriRobot/modules/writetool.py
@@ -14,55 +14,46 @@ async def handwrite(_, message: Message):
             message.chat.id, "`Please wait...,\n\nWriting your text...`"
         )
         API = f"https://api.sdbots.tk/write?text={text}"
-        req = requests.get(API).url
-        caption = f"""
+        response = requests.get(API)
+        if response.status_code == 200:
+            await m.delete()
+            await Tashri.send_photo(
+                message.chat.id,
+                photo=response.content,
+                caption=f"""
 sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
 
 ✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
 🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
-❄ **ʟɪɴᴋ :** `{req}`
-"""
-        await m.delete()
-        await Tashri.send_photo(
-            message.chat.id,
-            photo=req,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{req}")]]
-            ),
-        )
+            """
+            )
     else:
         lol = message.reply_to_message.text
         m = await Tashri.send_message(
             message.chat.id, "`Please wait...,\n\nWriting your text...`"
         )
         API = f"https://api.sdbots.tk/write?text={lol}"
-        req = requests.get(API).url
-        caption = f"""
+        response = requests.get(API)
+        if response.status_code == 200:
+            await m.delete()
+            await Tashri.send_photo(
+                message.chat.id,
+                photo=response.content,
+                caption=f"""
 sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
 
 ✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
 🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
-❄ **ʟɪɴᴋ :** `{req}`
-"""
-        await m.delete()
-        await Tashri.send_photo(
-            message.chat.id,
-            photo=req,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{req}")]]
-            ),
-        )
+            """
+            )
 
 
 __mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
 
 __help__ = """
-
  ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴡʜɪᴛᴇ ᴘᴀɢᴇ ᴡɪᴛʜ ᴀ ᴘᴇɴ 🖊
 
 ❍ /write <ᴛᴇxᴛ> *:* ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
 
 ☆............𝙱𝚈 » [Tashri](https://t.me/Tashri2342)............☆
- """
+"""
